25aa7b269b59d6e8edc05c7381e569e40dcf606f
RafaDom10 <r-domingues@outlook.com>
2021-06-25 16:44:14 -0300

    project done

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,18 +1,34 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
+
 import '../styles/question.scss';
 
 type QuestionProps = {
-  content: string,
+  content: string;
   author: {
-    name: string,
-    avatar: string
-  },
-  children?: ReactNode
-};
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+}
 
-export function Question({content, author, children}: QuestionProps) {
+export function Question({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
